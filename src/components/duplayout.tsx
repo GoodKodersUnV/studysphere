5607aefaad1bd0f77cbab6ccd20b95a224f4bf37
kalyan <kalyantingani@gmail.com>
2024-03-22 15:14:45 +0530

    join-quiz

--- src/components/duplayout.tsx
+++ src/components/duplayout.tsx
@@ -1,7 +1,6 @@
 "use client"
 import { useState } from 'react';
 import Header from '../components/Header'
-import { Link } from 'lucide-react';
 import { LuLayoutDashboard } from "react-icons/lu";
 import { MdOutlineCreateNewFolder } from "react-icons/md";
 import { TbArrowsJoin2 } from "react-icons/tb";
@@ -43,7 +42,7 @@ export default function ({ children, currentUser }) {
                     {
                         links.map((link) => {
                             return (
-                                <div onClick={()=>router.push("/")} className='w-full cursor-pointer border p-2 flex h-[40px] hover:bg-gray-100 items-center gap-3' key={link.name}>
+                                <div onClick={()=>router.push(link.path)} className='w-full cursor-pointer border p-2 flex h-[40px] hover:bg-gray-100 items-center gap-3' key={link.name}>
                                     <h1 className='text-xl px-2'>{link.icon}</h1>
                                     <h1 className={`${open === false && "hidden"}`}>{link.name}</h1>
                                 </div>
@@ -51,7 +50,7 @@ export default function ({ children, currentUser }) {
                         })
                     }
                 </div>
-                <div className='w-[85vw]'>{children}</div>
+                <div className='w-full'>{children}</div>
             </div>
         </div>
     )
