a07533d57fe9d1770b1403f0a2ee9eb30844b024
kalyan <kalyantingani@gmail.com>
2024-03-22 20:23:13 +0530

    animations

--- src/app/create-quiz/page.tsx
+++ src/app/create-quiz/page.tsx
@@ -3,26 +3,33 @@
 import React, { useState } from 'react'
 import GenerateQuiz from './_components/GenerateQuiz'
 import FileUpload from './_components/FileUpload'
+import CreateRoomFooter from './_components/CreateRoomFooter'
+import { FaFileExport } from "react-icons/fa";
+import { GiArtificialHive } from "react-icons/gi";
+
 
 const page = () => {
 
   const [currentIndex, setCurrentIndex] = useState(0);
   const page = {
-    "0": <h1>Hi</h1>,
+    "0": <CreateRoomFooter/>,
     "1": <FileUpload />,
     "2": <GenerateQuiz />,
   }
 
   return (
-    <div className=''>
-      <div className="flex gap-20 p-5 justify-center items-center text-blue-500 hover:text-blue-700 cursor-pointer">
+    <div className='border'>
+      <div className="flex gap-20 p-5 justify-center items-center cursor-pointer">
         <div onClick={() => setCurrentIndex(1)}>
+          <FaFileExport/>
           Generate from a file
         </div>
         <div onClick={() => setCurrentIndex(2)}>
+          <GiArtificialHive/>
           Generate using Ai
         </div>
         <div onClick={() => setCurrentIndex(3)}>
+          
           Create Quiz Manually
         </div>
       </div>
