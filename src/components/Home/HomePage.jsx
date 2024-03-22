503d75b2ac5d7751852516f0b9290e040aecd8cb
Uday Sagar <udaysagar.mail@gmail.com>
2024-03-23 04:49:17 +0530

    display friends

--- src/components/Home/HomePage.jsx
+++ src/components/Home/HomePage.jsx
@@ -6,24 +6,29 @@ import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 function HomePage({ currentUser }) {
-
   const [profile, setProfile] = useState([]);
   useEffect(() => {
     const getProfile = async () => {
       const res = await axios.post("/api/get-quizzes-taken", {
-        userId: currentUser.id
-      })
-      setProfile(res.data)
-    }
-    getProfile()
-  })
+        userId: currentUser.id,
+      });
+      setProfile(res.data);
+    };
+    getProfile();
+  });
   const router = useRouter();
   return (
     <div className="m-5 border flex h-full">
       <div className="w-[75%] p-5">
         <div className="flex gap-4">
           <div>
-            <Image src={currentUser.image} width={100} height={100} className="rounded" alt="profile" />
+            <Image
+              src={currentUser.image}
+              width={100}
+              height={100}
+              className="rounded"
+              alt="profile"
+            />
           </div>
           <div className="flex flex-col justify-between">
             <h1>{currentUser.email}</h1>
@@ -32,31 +37,47 @@ function HomePage({ currentUser }) {
             <h1>{currentUser.createdAt.toString()}</h1>
           </div>
         </div>
-        <table className='w-1/2 m-auto mt-12'>
+        <table className="w-1/2 m-auto mt-12">
           <thead>
             <tr>
-              <th className='p-2 border-b'>id</th>
-              <th className='p-2 border-b'>Name</th>
-              <th className='p-2 border-b'>Points</th>
+              <th className="p-2 border-b">id</th>
+              <th className="p-2 border-b">Name</th>
+              <th className="p-2 border-b">Points</th>
             </tr>
           </thead>
           <tbody>
-            {
-              profile.map((quiz) => {
-                return (
-                  <tr onClick={() => router.push(`/manage-quiz/${quiz.id}`)} className='hover:bg-gray-100 cursor-pointer' key={quiz.id}>
-                    <td className='p-2 border-b'>{quiz.id}</td>
-                    <td className='p-2 border-b'>{quiz.Quiz.name}</td>
-                    <td className='p-2 border-b'>{quiz.points}</td>
-                  </tr>
-                )
-              })
-            }
+            {profile.map((quiz) => {
+              return (
+                <tr
+                  onClick={() => router.push(`/manage-quiz/${quiz.id}`)}
+                  className="hover:bg-gray-100 cursor-pointer"
+                  key={quiz.id}
+                >
+                  <td className="p-2 border-b">{quiz.id}</td>
+                  <td className="p-2 border-b">{quiz.Quiz.name}</td>
+                  <td className="p-2 border-b">{quiz.points}</td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>
       <div className="border p-5 w-[25%]">
         Friends
+        {currentUser.friends.map((friend,index) => {
+          return (
+            <div className="p-2 border-b flex items-center gap-2" key={index}>
+              <Image
+                src={friend.friendUser.image}
+                className="rounded-full"
+                width={25}
+                height={25}
+                alt="profile"
+              />
+              {friend.friendUser.name}
+            </div>
+          );
+        })}
       </div>
     </div>
   );
