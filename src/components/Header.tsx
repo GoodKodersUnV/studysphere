aed053a33ecadc5e9fb26fa359844dd49d9c9c22
Vighnesh <vangarivighnesh@gmail.com>
2024-03-26 21:19:53 +0530

    message page(incomplete)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,6 @@
+'use client'
 
+import { useRouter } from 'next/navigation';
 import UserProfile from './UserProfile';
 import logo from '/public/logo.png'
 import Image from 'next/image';
@@ -9,16 +11,18 @@ import Image from 'next/image';
 //   open
 // }
 
+
 export default function ({ currentUser}) {
 
   const access = currentUser?.role === 'admin' || currentUser?.role === 'owner'
+  const router = useRouter()
   
   return (
 
     <div className=' px-2 z-10 sticky top-0 header'>
       <div className='flex items-center justify-between p-1  '>
         <div className='text-2xl font-bold flex gap-1.5 items-center'>
-          <Image src={logo} width={'50'} className='-ml-3 p-0' height={'50'} alt='Logo' />
+          <Image onClick={()=>router.push('/')} src={logo} width={'50'} className='-ml-3 cursor-pointer p-0' height={'50'} alt='Logo' />
           <h1>StudySphere</h1>
         </div>
         <UserProfile currentUser={currentUser} access={access} />
