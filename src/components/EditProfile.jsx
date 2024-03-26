bc6d638657782d337792eed7eeb457d3b44a7b98
goutham4126 <goutham4126@gmail.com>
2024-03-26 17:25:50 +0530

    ui updated

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -49,96 +49,95 @@ const Page = ({currentUser }) => {
   }
 
   return (
-    <div className="flex flex-col sm:grid grid-cols-3 gap-4 p-10">
-      <div className="col-span-1">
-        <div className="flex flex-col items-center">
-          <img src={user?.image} alt="User Image" className="w-40 h-40 rounded-full mb-4 bg-slate-300" />
-          <label className="text-sm text-gray-600">Change Image URL:</label>
-          <input
-            type="text"
-            value={user?.image}
-            onChange={(event) => setUser({ ...user, image: event.target.value })}
-            className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
-          />
-          <div className="mt-4">
-            <label className="text-sm text-gray-600">ID:</label>
+      <div className="flex flex-col items-center m-3">
+            <img src={user?.image} alt="User Image" className="w-40 h-40 rounded-full bg-slate-300" />
+            <label className="text-md font-semibold text-gray-600 m-2">Change Image URL:</label>
             <input
               type="text"
-              value={user?.id}
-              disabled
-              className="w-full px-4 py-2 rounded bg-gray-100 border border-gray-300 focus:outline-none"
+              value={user?.image}
+              onChange={(event) => setUser({ ...user, image: event.target.value })}
+              className=" w-96 px-4 py-2 rounded border border-blue-200 focus:outline-none focus:border-blue-500"
             />
-            <label className="text-sm text-gray-600">Username:</label>
-            <input
-              type="text"
-              value={user?.username}
-              onChange={(event) => setUser({ ...user, username: event.target.value })}
-              className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
-            />
-            <label className="text-sm text-gray-600">Created At:</label>
-            <input
-              type="text"
-              value={user?.createdAt}
-              disabled
-              className="w-full px-4 py-2 rounded bg-gray-100 border border-gray-300 focus:outline-none"
-            />
-            <label className="text-sm text-gray-600">Updated At:</label>
-            <input
-              type="text"
-              value={user?.updatedAt}
-              disabled
-              className="w-full px-4 py-2 rounded bg-gray-100 border border-gray-300 focus:outline-none"
-            />
+        <div className="flex justify-center gap-3">
+          <div className="w-[40%] mt-2">
+              <label className="text-sm text-gray-600">ID:</label>
+              <input
+                type="text"
+                value={user?.id}
+                disabled
+                className="w-full px-4 py-2 rounded bg-gray-100 border border-gray-300 focus:outline-none"
+              />
+              <label className="text-sm text-gray-600">Username:</label>
+              <input
+                type="text"
+                value={user?.username}
+                onChange={(event) => setUser({ ...user, username: event.target.value })}
+                className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
+              />
+              <label className="text-sm text-gray-600">Created At:</label>
+              <input
+                type="text"
+                value={user?.createdAt}
+                disabled
+                className="w-full px-4 py-2 rounded bg-gray-100 border border-gray-300 focus:outline-none"
+              />
+              <label className="text-sm text-gray-600">Updated At:</label>
+              <input
+                type="text"
+                value={user?.updatedAt}
+                disabled
+                className="w-full px-4 py-2 rounded bg-gray-100 border border-gray-300 focus:outline-none"
+              />
           </div>
-        </div>
+          <div className="w-[40%] mt-3">
+            <form onSubmit={handleSubmit}>
+              <label className="block mb-1 text-sm">
+                Name:
+                <input
+                  type="text"
+                  value={user?.name}
+                  onChange={(event) => setUser({ ...user, name: event.target.value })}
+                  className="w-full px-4 py-2.5 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
+                />
+              </label>
+              <label className="block mb-1 text-sm">
+                Email:
+                <input
+                  type="email"
+                  value={user?.email}
+                  onChange={(event) => setUser({ ...user, email: event.target.value })}
+                  className="w-full px-4 py-2.5 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
+                />
+              </label>
+              <label className="block mb-2 text-sm">
+                Organisation:
+                <input
+                  type="text"
+                  value={user?.organisation}
+                  onChange={(event) => setUser({ ...user, organisation: event.target.value })}
+                  className="w-full px-4 py-2.5 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
+                />
+              </label>
+              <label className="block mt-8 text-sm">
+                Email Verified:
+                <input
+                  type="checkbox"
+                  checked={user?.emailVerified}
+                  onChange={(event) => setUser({ ...user, emailVerified: event.target.checked })}
+                  className="ml-2"
+                />
+              </label>
+            </form>
+          </div>
       </div>
-      <div className="col-span-2">
-        <form onSubmit={handleSubmit}>
-          <label className="block mb-2 text-sm">
-            Name:
-            <input
-              type="text"
-              value={user?.name}
-              onChange={(event) => setUser({ ...user, name: event.target.value })}
-              className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
-            />
-          </label>
-          <label className="block mb-2 text-sm">
-            Email:
-            <input
-              type="email"
-              value={user?.email}
-              onChange={(event) => setUser({ ...user, email: event.target.value })}
-              className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
-            />
-          </label>
-          <label className="block mb-2 text-sm">
-            Email Verified:
-            <input
-              type="checkbox"
-              checked={user?.emailVerified}
-              onChange={(event) => setUser({ ...user, emailVerified: event.target.checked })}
-              className="ml-2"
-            />
-          </label>
-          <label className="block mb-2 text-sm">
-            Organisation:
-            <input
-              type="text"
-              value={user?.organisation}
-              onChange={(event) => setUser({ ...user, organisation: event.target.value })}
-              className="w-full px-4 py-2 rounded border border-gray-300 focus:outline-none focus:border-blue-500"
-            />
-          </label>
-          <button
-            type="submit"
-            className="bg-gray-900 hover:bg-gray-800 text-white text-sm font-semibold py-2 px-3 rounded-lg mt-4 mx-auto block"
-          >
-            Update
-          </button>
-        </form>
-      </div>
+      <button
+                type="submit"
+                className="bg-gray-900 hover:bg-gray-800 text-white text-sm font-semibold py-2 px-3 rounded-lg mt-4 mx-auto block"
+              >
+                Update
+           </button>
     </div>
+
   );
 };
 
