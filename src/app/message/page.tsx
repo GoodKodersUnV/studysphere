73b85b4593fb1b9e524513e4ea93cd7d175dd48a
Uday Sagar <udaysagar.mail@gmail.com>
2024-03-27 16:54:42 +0530

    messages page

---
+++ src/app/message/page.tsx
@@ -0,0 +1,12 @@
+
+const page = () => {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <div className="text-center text-2xl font-medium">
+        Select a friend to start chat
+      </div>
+    </div>
+  )
+}
+
+export default page
