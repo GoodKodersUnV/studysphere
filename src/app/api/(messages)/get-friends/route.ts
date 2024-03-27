edf8309395e94d9df1a477f24a362331b9d183b9
Uday Sagar <udaysagar.mail@gmail.com>
2024-03-27 21:31:45 +0530

    fix messages route

--- src/app/api/(messages)/get-friends/route.ts
+++ src/app/api/(messages)/get-friends/route.ts
@@ -2,14 +2,15 @@ import { NextResponse } from "next/server";
 import { db } from "@/libs/db";
 import getCurrentUser from "@/actions/getCurrentUser";
 
-export async function GET() {
+export async function GET(req: Request) {
+
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser) {
+    return NextResponse.redirect(new URL("/signin", req.url));
+  }
+
   try {
-    const currentUser = await getCurrentUser();
-
-    if (!currentUser) {
-      return NextResponse.json({ friendsList: [], currentUser: null });
-    }
-
     const friends = await db.user.findUnique({
       where: {
         id: currentUser.id,
