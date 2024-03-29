e9da2ed0a8dbaece26f44e22416ea86056db0ef1
Uday Sagar <udaysagar.mail@gmail.com>
2024-03-29 22:41:56 +0530

    notifications feature

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,29 +4,58 @@ import { useRouter } from 'next/navigation';
 import UserProfile from './UserProfile';
 import logo from '/public/logo.png'
 import Image from 'next/image';
-// type newUser = User
+import { useEffect, useState } from 'react';
+import NotificationModal from './NotificationModal';
+import { IoNotificationsCircle } from "react-icons/io5";
 
-// interface Props {
-//   currentUser?: newUser | null;
-//   open
-// }
-
-
-export default function ({ currentUser}) {
+export default function Header({ currentUser }: { currentUser: any }) {
 
   const access = currentUser?.role === 'admin' || currentUser?.role === 'owner'
   const router = useRouter()
-  
-  return (
 
+  const [showNotification, setShowNotification] = useState(false)
+  const [notifications, setNotifications] = useState(currentUser?.notifications.filter((notification) => !notification.readAt) || [])
+  useEffect(() => {
+    const handleClickOutside = (event: any) => {
+      if (event.target.closest('.header')) {
+        return;
+      }
+      setShowNotification(false);
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [showNotification]);
+
+  return (
     <div className=' px-2 z-10 sticky top-0 header'>
       <div className='flex items-center justify-between p-1  '>
         <div className='text-2xl font-bold flex gap-1.5 items-center'>
-          <Image onClick={()=>router.push('/')} src={logo} width={'50'} className='-ml-3 cursor-pointer p-0' height={'50'} alt='Logo' />
+          <Image onClick={() => router.push('/')} src={logo} width={'50'} className='-ml-3 cursor-pointer p-0' height={'50'} alt='Logo' />
           <h1>StudySphere</h1>
         </div>
-        <UserProfile currentUser={currentUser} access={access} />
+        <div className='flex items-center gap-5 relative'>
+          <div className=' relative' >
+            <div className='cursor-pointer'>
+              <IoNotificationsCircle size={30} onClick={() => setShowNotification(!showNotification)} />
+            </div>
+            {
+              notifications.length > 0 && <div className='absolute -top-2 -right-2 bg-red-400 text-white rounded-full w-5 h-5 flex items-center justify-center'>
+                {notifications.length}
+              </div>
+            }
+            {showNotification && (
+              <div className='absolute right-0 mt-2'>
+                <NotificationModal notifications={notifications} setNotifications={setNotifications} />
+              </div>
+            )}
+          </div>
+          <UserProfile currentUser={currentUser} access={access} />
+        </div>
       </div>
     </div>
-  )
+  );
 }
