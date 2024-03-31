c04fa1e2513112e1ebb5a830c83a83a0d43089e8
kalyan <kalyantingani@gmail.com>
2024-03-31 19:11:52 +0530

    bug fix pro plan

--- src/app/create-quiz/page.tsx
+++ src/app/create-quiz/page.tsx
@@ -1,11 +1,28 @@
-import getCurrentUser from "@/actions/getCurrentUser"
-import CreateQuiz from "./CreateQuiz"
+"use client"
+import { useEffect, useState } from "react";
+import CreateQuiz from "./CreateQuiz";
+import axios from "axios";
 
-const page = async () => {
-  const currentUser = await getCurrentUser()
+const Page = () => {
+  const [token, setToken] = useState("fetching...");
+
+  useEffect(() => {
+    const getTokens = async () => {
+      try {
+        const res = await axios.get("/api/get-tokens");
+        setToken(res.data.token);
+      } catch (error) {
+        console.error("Error fetching tokens:", error);
+        setToken("Error fetching tokens");
+      }
+    };
+
+    getTokens();
+  }, []);
+
   return (
-    <CreateQuiz currentUser={currentUser} />
-  )
-}
+    <CreateQuiz token={token} />
+  );
+};
 
-export default page
+export default Page;
