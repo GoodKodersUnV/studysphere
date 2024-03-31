ab34c53169673aace9b09419934e8162619ffe98
kalyan <kalyantingani@gmail.com>
2024-03-31 21:08:43 +0530

    check?

--- src/app/api/(premium)/get-user-tokens/route.tsx
+++ src/app/api/(premium)/get-user-tokens/route.tsx
@@ -1,13 +1,18 @@
 import getCurrentUser from "@/actions/getCurrentUser";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function POST(req : NextRequest) {
+    
     const currentUser = await getCurrentUser();
+    if (!currentUser) {
+        return NextResponse.redirect(new URL("/signin", req.url));
+      }
 
     const currentTime = new Date().getTime();
     if (currentUser?.plan && (new Date(currentUser?.plan?.endTime).getTime() > currentTime)) {
         return NextResponse.json({ token: "pro", endTime: new Date(currentUser?.plan?.endTime).toLocaleString() });
-    } else {
-        return NextResponse.json({ token: currentUser?.tokens.toString()});
-    }
+    } 
+
+    return NextResponse.json({ token: currentUser.tokens.toString()});
+    
 }
